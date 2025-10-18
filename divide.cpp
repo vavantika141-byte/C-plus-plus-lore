ade78b3df1b835a02a04046c360783f2146fc9b3
Avantika verma <vavantika141@gmail.com>
2025-10-18 17:16:59 +0530

    divideee

---
+++ divide.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, divide ;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    divide = a /  b;
+
+    cout << "The divide is: " << divide<< endl;
+
+    return 0;
+}
