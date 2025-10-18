98d1da613cc4d2feaabfcddf52cc74cc1961092b
Avantika verma <vavantika141@gmail.com>
2025-10-18 17:05:50 +0530

    started my c++ code

---
+++ multiply.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, multiply ;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    multiply = a * b;
+
+    cout << "The sum is: " << multiply << endl;
+
+    return 0;
+}
